29ffab9686eb7e24a066292860c97a4ff3512425
oerlex <alex.sp@posteo.de>
2021-07-01 23:04:21 +0200

    Update vmSetup.sh

--- vmSetup.sh
+++ vmSetup.sh
@@ -68,7 +68,7 @@ pip3 install objection
 apt install exa
 
 curl -s https://api.github.com/repos/Peltoche/lsd/releases/latest \
-| grep "browser_download_url.*deb" \
+| grep "https://github.com/Peltoche/lsd/releases/download/*/lsd_*amd64.*deb" \
 | cut -d : -f 2,3 \
 | tr -d \" \
 | wget -qi -
