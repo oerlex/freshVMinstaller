19a1b099c9e26a0068b80e1cced3c0367e940460
alex <alexander.rautenberg@appgate.com>
2022-02-04 15:51:06 +0100

    Updated tools much todo though

--- vmSetup.sh
+++ vmSetup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-#Clone my repo
-#cd
-#git clone https://github.com/oerlex/freshVMinstaller.git
-
+# This is for Debian 64bit systems
 
 #Clean up
 sudo rm -r /usr/local/go
@@ -12,6 +9,7 @@ sed -i '/usr\/local\/go\/bin/d' ~/.profile
 tail -n 4 ~/.profile
 sudo dpkg -r atom
 
+# Most prorams gonna be installed here
 mkdir ~/programs
 cd ~/programs
 
@@ -20,32 +18,25 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt install python3-pip
 sudo apt install snap
-sudo apt -y kali-linux-everything
 
+#### Language Support
 
 # Install Go
-wget https://golang.org/dl/go1.15.6.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.15.6.linux-amd64.tar.gz
+#TODO Latest version
+wget https://golang.org/dl/go1.17.6.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.17.6.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
 source ~/.profile
 go version
 
-# Install Bloodhound
-sudo apt install bloodhound
-
-
+# Misc
+sudo apt install socat
+sudo apt install nmap
+python3 -m pip install semgrep
+sudo apt install wireshark
 # Install ffuf
 go get -u github.com/ffuf/ffuf
 sudo cp ~/go/bin/ffuf /opt/
-
-# Install atom
-curl -s https://api.github.com/repos/atom/atom/releases/latest \
-| grep "browser_download_url.*deb" \
-| cut -d : -f 2,3 \
-| tr -d \" \
-| wget -qi -
-sudo dpkg -i atom*.deb
-
 
 # Install tmux
 sudo apt install tmux
@@ -56,13 +47,57 @@ cp ~/freshVMinstaller/.tmux.conf.local  ~/.tmux.conf.local
 tmux source-file ~/.tmux.conf
 
 
+##### Code Auditing
+# CodeQL
+mkdir ~/codeql-home
+cd ~/codeql-home
+DOWNLOAD_URL=$(curl -s https://api.github.com/repos/github/codeql-cli-binaries/releases/latest \
+        | grep browser_download_url \
+        | grep codeql-linux64.zip \
+        | cut -d '"' -f 4)
+curl -s -L -o ./codeql-latest.zip "$DOWNLOAD_URL"
+unzip codeql-latest.zip
+git clone https://github.com/github/codeql
+mv codeql codeql-repo
+cd ~/programs
+
+
+##### Reversing
+# GHIDRA
+DOWNLOAD_URL=$(curl -s https://api.github.com/repos/NationalSecurityAgency/ghidra/releases/latest \
+        | grep browser_download_url \
+        | grep ghidra-latest.zip \
+        | cut -d '"' -f 4)
+curl -s -L -o ./ghidra-latest.zip "$DOWNLOAD_URL"
+
+##### EDITORS
+
+# Install atom
+wget https://atom.io/download/deb
+sudo dpkg -i atom-amd64.deb
+
+# VSCode
+TEMP_DEB="$(mktemp)" &&
+wget -O "$TEMP_DEB" 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64' &&
+sudo dpkg -i "$TEMP_DEB"
+rm -f "$TEMP_DEB"
+
+
+
+
+
+
+
+
+
+##### Mobile
+#  Android Studio
+#sudo snap install --classic android-studio
+
+
 # Install MobSF
-git clone https://github.com/MobSF/Mobile-Security-Framework-MobSF.git
-Mobile-Security-Framework-MobSF/setup.sh
-
-
-# Install Android Studio
-sudo snap install --classic android-studio
+#git clone https://github.com/MobSF/Mobile-Security-Framework-MobSF.git
+#Mobile-Security-Framework-MobSF/setup.sh
 
 
 # Install frida+objection
@@ -75,9 +110,13 @@ cd /opt
 git clone https://github.com/danielmiessler/SecLists.git
 
 
-# Windows / AD / Powershell
+##### Windows / AD / Powershell
 
-## Powersploit & PSTools
+# Install Bloodhound
+sudo apt install bloodhound
+
+
+# Powersploit & PSTools
 cd /opt/
 https://github.com/PowerShellMafia/PowerSploit.git
 mkdir PSTools
@@ -86,10 +125,9 @@ wget https://download.sysinternals.com/files/PSTools.zip
 unzip PSTools.zip
 
 
-## impacket
+# impacket
 curl -s https://api.github.com/repos/SecureAuthCorp/impacket/releases/latest \
 | grep "browser_download_url.*tar.gz" \
 | cut -d : -f 2,3 \
 | tr -d \" \
 | wget -qi -
-
